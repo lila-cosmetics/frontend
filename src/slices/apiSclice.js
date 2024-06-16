44dca8e236476ac51a46867bbdcac69cc44008b9
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2024-06-16 15:36:42 +0200

    updates on redux boilerplate

--- src/slices/apiSclice.js
+++ src/slices/apiSclice.js
@@ -1,8 +1,12 @@
-import { CreateApi, createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
+//the root api slice
 //fetchBaseQuery is a function allows us to make req to backend api
-import { BASE_URL } from "../api/productsAPI";
+
+import { BASE_URL } from "../api/api";
 
 export const apiSlice = createApi({
-    
-})
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  tagTypes: ["Product", "Order", "User"], //the types of data we are fetching from our api
+  endpoints: (builder) => ({}),
+});
--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSclice";
 
 const store = configureStore({
-    reducer:{},
+    reducer:{
+        [apiSlice.reducerPath]:apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware)=> getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools:true
 })
 
 
