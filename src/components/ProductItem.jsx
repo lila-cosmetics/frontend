a2698e3f2a40f45cb3f2d70c427b3cf28cf9fa91
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2024-06-28 11:44:39 +0200

    selecting number of product and add it to cart

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,19 +1,31 @@
-import { useParams, Link } from "react-router-dom";
+import { Label, Select } from "flowbite-react";
+import { useParams, Link ,useNavigate} from "react-router-dom";
+import { useDispatch } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "./Message";
 import { useGetProductDetailsQuery } from "../slices/productApiSlice";
+import { useState } from "react";
+import { addToCart } from "../slices/cartSlice";
 
 function ProductItem() {
   const { id: productId } = useParams();
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const addToCartHandler=()=>{
+    //we need addtocart action from cartslice and then dispatch addtocart action
+    dispatch(addToCart({...products, qty}))
+    navigate('/cart')
+  }
   const {
     data: products,
     isLoading,
     error,
   } = useGetProductDetailsQuery(productId);
-  console.log('products:', products, 'isLoading:', isLoading, 'error:', error);
-  console.log('Product ID:', productId);
-  console.log('API Endpoint:', `/api/products/${productId}`);
-  
+  console.log("products:", products, "isLoading:", isLoading, "error:", error);
+  console.log("Product ID:", productId);
+  console.log("API Endpoint:", `/api/products/${productId}`);
+
   return (
     <div className="flex flex-col gap-5 mt-20 ml-48 mb-52">
       <div>
@@ -22,28 +34,51 @@ function ProductItem() {
         </Link>
       </div>
 
-
       {isLoading ? (
-        <Loader/>
-      ): error ? (
+        <Loader />
+      ) : error ? (
         <Message>{error?.data?.message || error.error}</Message>
-      ) : ( <div className="flex gap-10">
-        <img
-          src={products.image}
-          alt={products.name}
-          style={{ width: "400px", height: "600px" }}
-        />
-        <div className="flex flex-col gap-10">
-          <h2>{products.name}</h2>
-          <p>{products.description}</p>
+      ) : (
+        <div className="flex gap-10">
+          <img
+            src={products.image}
+            alt={products.name}
+            style={{ width: "400px", height: "600px" }}
+          />
+          <div className="flex flex-col gap-10">
+            <h2>{products.name}</h2>
+            <p>{products.description}</p>
 
-          <span>{products.price} €</span>
-          <div>
-            <button className="border p-3 bg-slate-100">Add to Cart</button>
+            {products.availableQuantity > 0 ?(
+              <div className="w-1/3">
+                <Label
+                  htmlFor="countInStock"
+                  value="Select the number of product"
+           
+                />
+                <Select id="countInStock" required
+                       defaultValue={1}
+                       onChange={(e)=>setQty(e.target.value)}>
+                  {/* show maximum 10 count is stock
+                If products.availableQuantity is less than 10, it uses products.availableQuantity. Otherwise, it uses 10. */}
+                  {[
+                    ...Array(Math.min(products.availableQuantity, 10)).keys(),
+                  ].map((x) => (
+                    <option key={x +1} value={x + 1}>{x + 1}</option>
+                  ))}
+                </Select>
+              </div>
+            ):(
+              <p>Out of stock</p>
+            )}
+
+            <span>{products.price} €</span>
+            <div>
+              <button className="border p-3 bg-slate-100" onClick={addToCartHandler}>Add to Cart</button>
+            </div>
           </div>
         </div>
-      </div>)}
-     
+      )}
     </div>
   );
 }
--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -1,13 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
+/* This line defines the initial state for the cart. It checks if there is an existing cart stored in the browser's localStorage. If there is, it parses the JSON string into an object and uses it as the initial state.
+If no cart is found in localStorage, it sets the initial state to an object with an empty cartSlice array. */
 
-
-const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {cartSlice:[]}
+//helper function
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
 
 const cartSlice = createSlice({
-    name:'cart',
-    initialState,
-    reducers:{}   //any functions that have to do with the cart(add to cart/remove..)
-})
+  name: "cart",
+  initialState,
+  //current state of the cart , action:any data inside of payload
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
 
-export default cartSlice.reducer
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      //calculating item price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+
+      //calculating shipping price (if order is over 20€ then free, else 3€)
+      state.shippingPrice = addDecimals(state.itemsPrice > 20 ? 0 : 3);
+
+      //tax price (%15)
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+
+      //calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  }, //any functions that have to do with the cart(add to cart/remove..)
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
